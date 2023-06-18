80c66e93e7ee30ffabb3ad1dfc52038781bb0520
Sam <sam.finchett@outlook.com>
2023-06-18 16:09:17 +0800

    fix debugging

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <gtk/gtk.h>
+#include "chip8_application.h"
+#include <stdio.h>
 
-static void activate(GtkApplication *app, gpointer user_data)
+int main (int argc, char *argv[])
 {
-  GtkWidget *window;
+    Chip8Application *chip8;
+    chip8 = chip8_application_new();
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);
-  gtk_widget_set_visible(window, true);
-}
 
-int main(int argc, char **argv)
-{
-  GtkApplication *app;
-  int status;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
+    g_object_unref (chip8);
 
-  return status;
+    return 0;
 }
