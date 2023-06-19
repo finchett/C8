a6d5cd0bec0d18548e6541c8d3d41f69f41cc60b
Sam <sam.finchett@outlook.com>
2023-06-19 16:02:58 +0800

    use GLEW loading lib

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 #include "chip8_application.h"
 #include <stdio.h>
-#include <GL/glut.h>
+#include <GL/glew.h>
 
 static bool
 render (GtkGLArea *area, GdkGLContext *context)
@@ -12,30 +12,15 @@ render (GtkGLArea *area, GdkGLContext *context)
   // already been set to be the size of the allocation
 
   // we can start by clearing the buffer
-  glClearColor (0, 0, 0, 0);
+  // coloring to check if working.
+  glClearColor (90, 0, 0, 0);
   glClear (GL_COLOR_BUFFER_BIT);
-
-
+  
   // we completed our drawing; the draw commands will be
   // flushed at the end of the signal emission chain, and
   // the buffers will be drawn on the window
   return TRUE;
 }
-
-
-static void setup_glarea (GtkWindow *window)
-{
-  // create a GtkGLArea instance
-  GtkWidget *gl_area = gtk_gl_area_new ();
-
-  g_signal_connect (gl_area, "render", G_CALLBACK (render), NULL);
-
-
-  gtk_window_set_child(GTK_WINDOW(window), gl_area);
-  gtk_widget_set_visible(GTK_GL_AREA(gl_area), TRUE);
-
-  // connect to the "render" signal
-};
 
 // Managing window may not belong here.
 static void activate (Chip8Application *app)
@@ -44,10 +29,15 @@ static void activate (Chip8Application *app)
 
 
   window = gtk_application_window_new (GTK_APPLICATION (app));
-  setup_glarea (GTK_WINDOW (window));
-
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
+
+
+  // create a GtkGLArea instance
+  GtkWidget *gl_area = gtk_gl_area_new ();
+  g_signal_connect (gl_area, "render", G_CALLBACK (render), NULL);
+  gtk_window_set_child(GTK_WINDOW(window), gl_area);
+
   gtk_window_present (GTK_WINDOW (window));
 
 };
@@ -69,10 +59,12 @@ int main (int argc, char *argv[])
     
     todo: refactore for readability.
       - makes app
-      - make window
-      - make widget
 
-      - connect signals
+      - attach activate signal that will:
+        - make window
+        - make widget
+        - connect signals
+
       - run
     
     */
