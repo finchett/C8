6b00453e05737f404f09b35fd9b5fb20dc72e67c
Sam <sam.finchett@outlook.com>
2023-06-22 12:53:24 +0800

    add stack

--- memory.c
+++ memory.c
@@ -1,3 +1,15 @@
 #include <stdint.h>
+#include "memory.h"
 
 int16_t memory[4096];
+int16_t stack[32];
+
+static int stack_index = 0;
+
+void push(int16_t n) {
+    stack[stack_index++] = n;
+};
+
+int16_t pop() {
+    return stack[stack_index--];
+};
--- memory.h
+++ memory.h
@@ -2,3 +2,7 @@
 #include <stdint.h>
 
 extern int16_t memory[];
+extern int16_t stack[];
+
+void push(int16_t n);
+int16_t pop();
